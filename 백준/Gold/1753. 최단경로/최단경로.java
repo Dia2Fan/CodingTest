7efa6bae00bfa517064e128e74293ebbd01dfe49
Dia2Fan <167517205+Dia2Fan@users.noreply.github.com>
2024-05-20 21:59:48 +0900

    [Gold IV] Title: 최단경로, Time: 2080 ms, Memory: 324116 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,48 +1,58 @@
 import java.util.*;
 
 public class Main {
-    static class Pair {
-        int node;
-        int weight;
+    static class Node implements Comparable<Node>{
+       int toNext;
+       int toLength;
 
-        Pair(int node, int weight) {
-            this.node = node;
-            this.weight = weight;
+        public Node(int toNext, int toLength) {
+            this.toNext = toNext;
+            this.toLength = toLength;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.toLength<o.toLength ? -1 : (this.toLength==o.toLength ? 0 : 1);
         }
     }
-
     static int[] minDistance;
-    static ArrayList<Pair>[] adjList;
-
+    static boolean[] visited;
+    static ArrayList<Node>[] nodeList;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int nodeNumber = sc.nextInt();
         int edgeNumber = sc.nextInt();
         int startPoint = sc.nextInt();
-        adjList = new ArrayList[nodeNumber+1];
-        for (int i=0;i<=nodeNumber;i++){
-            adjList[i] = new ArrayList<>();
+        nodeList= new ArrayList[nodeNumber+1];
+        for (int i=1;i<=nodeNumber;i++){
+            nodeList[i]=new ArrayList<>();
         }
         minDistance = new int[nodeNumber+1];
+        visited = new boolean[nodeNumber+1];
         Arrays.fill(minDistance,100000000);
         minDistance[startPoint]=0;
         for (int i = 0; i < edgeNumber; i++) {
             int nodeNum = sc.nextInt();
-            int nextNode = sc.nextInt();
-            int weight = sc.nextInt();
-            adjList[nodeNum].add(new Pair(nextNode, weight));
+            nodeList[nodeNum].add(new Node(sc.nextInt(), sc.nextInt()));
+
         }
-        PriorityQueue<Pair> queue = new PriorityQueue<>(Comparator.comparingInt(a -> a.weight));
-        queue.offer(new Pair(startPoint, 0));
-        while(!queue.isEmpty()){
-            Pair current = queue.poll();
-            if (current.weight > minDistance[current.node]) continue;
-            for (Pair edge : adjList[current.node]) {
-                int newDistance = current.weight + edge.weight;
-                if (newDistance < minDistance[edge.node]) {
-                    minDistance[edge.node] = newDistance;
-                    queue.offer(new Pair(edge.node, newDistance));
-                }
+        PriorityQueue<Node> deque = new PriorityQueue<>();
+        for (int i = 0; i <minDistance.length; i++) {
+            if(minDistance[i]==0){
+                deque.offer(new Node(startPoint,0));
+            }
+        }
+        while(!deque.isEmpty()){
+            Node current = deque.poll();
+           if(visited[current.toNext]) continue;
+           visited[current.toNext] = true;
+            for (Node node : nodeList[current.toNext]) {
+                int newDistance = node.toLength ;
+                int newNext = node.toNext;
+                if(minDistance[newNext]>minDistance[current.toNext]+newDistance){
+                    minDistance[newNext] = minDistance[current.toNext]+newDistance;
+                    deque.offer(new Node(newNext, minDistance[newNext]));
+                }//최소거리 조정부분 주의하기!
             }
         }
         for (int i =1;i< minDistance.length;i++) {
@@ -52,5 +62,6 @@ public class Main {
                 System.out.println("INF");
             }
         }
+
     }
 }
