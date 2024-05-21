61f3445ffc202bceca75907bb1ef47f98a5bbfae
Dia2Fan <167517205+Dia2Fan@users.noreply.github.com>
2024-05-21 21:44:47 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 2064 ms, Memory: 268780 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class Main {
+    static class Node implements Comparable<Node>{
+        int start ;
+        int end ;
+        int value;
+        public Node(int start,int end, int value){
+            this.start =start;
+            this.end =end;
+            this.value = value;
+        }
+
+        public int compareTo(Node o){
+            return this.value - o.value;
+        }
+    }
+    static int[] node;
+    static ArrayList<Node>[] nodeList;
+    static int totalValue=0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nodeNumber = sc.nextInt();
+        int edgeNumber = sc.nextInt();
+        node = new int[nodeNumber+1];
+        nodeList = new ArrayList[nodeNumber+1];
+        for (int i = 1; i <= nodeNumber; i++) {
+            node[i]=i;
+            nodeList[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < edgeNumber; i++) {
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+            int value = sc.nextInt();
+            nodeList[start].add(new Node(start,end,value));
+            nodeList[end].add(new Node(end,start,value)); // 양방향 간선 추가
+        }
+        ArrayList<Node> nodeOrder = new ArrayList<>();
+        for (ArrayList<Node> nodes : nodeList) {
+            if(nodes!=null)nodeOrder.addAll(nodes);
+        }
+        Collections.sort(nodeOrder);
+        for (int i = 0; i < nodeOrder.size(); i++) { // 모든 간선을 확인
+            if(find(nodeOrder.get(i).start)!=find(nodeOrder.get(i).end)){
+                union(nodeOrder.get(i).start,nodeOrder.get(i).end);
+                totalValue+=nodeOrder.get(i).value;
+            }
+        }
+        System.out.println(totalValue);
+    }
+    public static int find(int i){
+        if(node[i]==i){
+            return i;
+        }else{
+           return node[i]=find(node[i]);
+        }
+    }
+    public static void union(int i1, int i2){
+        int newi1=find(i1);
+        int newi2=find(i2);
+        node[newi1] = newi2;
+    }
+}
