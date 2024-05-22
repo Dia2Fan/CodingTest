95747b9c756bf548a4db527b018cc53ff3411341
Dia2Fan <167517205+Dia2Fan@users.noreply.github.com>
2024-05-23 08:48:37 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 212 ms, Memory: 17520 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,33 +1,24 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+    static int[][] apartment = new int[15][15];
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int T = scanner.nextInt();
-        for (int i = 1; i <= T; i++) {
-            int K = scanner.nextInt();
-            int N = scanner.nextInt();
-            System.out.println(apartmentPeople(K, N));
+        Scanner sc = new Scanner(System.in);
+        int questionNumber = sc.nextInt();
+        for (int i = 1; i <= 14; i++) {
+            apartment[0][i]=i;
         }
-    }
-
-    public static int apartmentPeople(int k, int n) {
-        int people=0;
-        int[][] apartment = new int[k+1][n];
-        for (int i=0;i<k+1;i++) {
-            for (int j=0;j<n;j++) {
-                apartment[0][j] = j+1;
-                apartment[i][0] =1;
-            }
-
-        }
-
-        for (int i=1;i<k+1;i++) {
-            for (int j=1;j<n;j++) {
+        for (int i = 1; i <=14; i++) {
+            for (int j = 2; j <= 14; j++) {
+                apartment[i][1]=1;
                 apartment[i][j]=apartment[i][j-1]+apartment[i-1][j];
             }
         }
-        people=apartment[k][n-1];
-        return people;
+        for (int i = 0; i < questionNumber; i++) {
+            int floor = sc.nextInt();
+            int number = sc.nextInt();
+            System.out.println(apartment[floor][number]);
+        }
     }
+
 }
