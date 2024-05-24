0ce1b681a2905eb8064755f2946eb92129386f67
Dia2Fan <167517205+Dia2Fan@users.noreply.github.com>
2024-05-24 21:52:12 +0900

    [Silver III] Title: 1로 만들기, Time: 240 ms, Memory: 21644 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,34 +1,17 @@
 import java.util.*;
-
+//코드 리펙토링
 public class Main {
     static int[] minCheck;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         minCheck = new int[N+1];
-        for (int i = 1; i <= N; i++) {
-            if(minCheck[i]!=0){
-                minCheck[i]=Math.min(minCheck[i],minCheck[i-1]+1);
-            }else{
-                minCheck[i]=minCheck[i-1]+1;
-            }
-            if(2*i<=N){
-                if(minCheck[2*i]!=0){
-                    minCheck[2*i]=Math.min(minCheck[i]+1,minCheck[2*i]);
-                }else{
-                    minCheck[2*i]=minCheck[i]+1;
-                }
-            }
-            if(3*i<=N){
-                if(minCheck[3*i]!=0){
-                    minCheck[3*i]=Math.min(minCheck[i]+1,minCheck[3*i]);
-                }else{
-                    minCheck[3*i]=minCheck[i]+1;
-                }
-            }
+        for (int i = 2; i <= N; i++) {
+            minCheck[i]=minCheck[i-1]+1;
+            if(i%2==0)minCheck[i]=Math.min(minCheck[i],minCheck[i/2]+1);
+            if(i%3==0)minCheck[i]=Math.min(minCheck[i],minCheck[i/3]+1);
         }
-
-        System.out.println(minCheck[N]-1);
+        System.out.println(minCheck[N]);
     }
 }
 
